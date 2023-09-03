df6bde015cdeb733811247c3c2e85797812c8496
shreejeshua <shreejeshua@gmail.com>
2023-09-03 21:18:55 +0530

    initial commit

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-    strings:["Mearn Stack Developer", "Web Developer" ,"Front-end devloper"],
+    strings:["Mearn Stack Developer", "Web Developer" ,"Front-end developer"],
     typeSpeed:100,
     backSpeed:100,
       backDelay:1000,
